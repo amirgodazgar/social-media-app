2ddd0f3121d2d27d82167996e549544bc51a7fea
amir.godazgar <amir.godazgar@connect2wow.com>
2022-12-06 09:34:56 +0330

    add infinite scroll part 1

--- src/screens/feed/feed.js
+++ src/screens/feed/feed.js
@@ -5,12 +5,29 @@ import {getPosts} from '../../services/getPosts';
 import Layout from '../../components/layout/layout';
 import Post from './post';
 import {SCREEN_NAMES} from '../../constant/screenRoutes';
-import {useQuery} from 'react-query';
+import {useInfiniteQuery, useQuery} from 'react-query';
 import Indicator from '../../components/indicator/indicator';
+import httpsPrivate from '../../services/https';
 
 const Feed = () => {
   const navigation = useNavigation();
-  const {data: postsData, isLoading} = useQuery('getPosts', getPosts);
+
+  const {isLoading, isFetchingNextPage, data, hasNextPage, fetchNextPage} =
+    useInfiniteQuery('posts-infinite', getPosts, {
+      getNextPageParam: (lastPage, allPage) => {
+        console.log('iiiiii', !lastPage.length);
+
+        if (!lastPage.length) console.log('EMPTY');
+        else return lastPage;
+      },
+    });
+
+  console.log('infiniteData', data && data.pages.flat());
+
+  const loadMore = () => {
+    // console.log('hasss', hasNextPage);
+    if (hasNextPage) fetchNextPage();
+  };
 
   if (isLoading) return <Indicator />;
 
@@ -43,9 +60,12 @@ const Feed = () => {
   return (
     <Layout storyMode>
       <FlatList
-        data={postsData}
+        data={data.pages.flat()}
         renderItem={renderItem}
         keyExtractor={item => item.id}
+        onEndReached={loadMore}
+        onEndReachedThreshold={0.3}
+        // ListFooterComponent={isFetchingNextPage ? <Indicator /> : null}
       />
     </Layout>
   );
--- src/services/getPosts.js
+++ src/services/getPosts.js
@@ -1,10 +1,10 @@
 import {getPostsMapper} from '../screens/feed/mapper/get-post-mapper';
 import httpsPrivate from './https';
 
-export const getPosts = async () => {
+export const getPosts = async ({pageParam = 1}) => {
   try {
     const response = await httpsPrivate.get(
-      '/posts?populate[0]=images&populate[1]=user&sort[0]=createdAt:desc',
+      `/posts?populate[0]=images&populate[1]=user&sort[0]=createdAt:desc&pagination[page]=${pageParam}&pagination[pageSize]=2`,
     );
     const mappedResponse = getPostsMapper(response.data.data);
     return mappedResponse;
